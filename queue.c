e476d5bdadf677dac7289abf42722b72fe0e3b5d
Dalerjon Murodov <54956921+dalerjon@users.noreply.github.com>
2021-02-19 13:06:51 -0500

    A queue that can push using FIFO or priority

---
+++ queue.c
@@ -0,0 +1,92 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "queue.h"
+
+Node* newNode(int d, int p)
+{
+    Node* temp = (Node*)malloc(sizeof(Node)); 
+    temp->data = d; 
+    temp->priority = p;
+    temp->next = NULL;
+ 
+    return temp; 
+} 
+ 
+int peek(Node** head)
+{ 
+    return (*head)->data; 
+}
+
+Node* getNode(Node** head)
+{
+    return (*head);
+}
+
+void pop(Node** head) 
+{
+    Node* temp = *head;
+    (*head) = (*head)->next;
+    free(temp);
+}
+ 
+void push(Node** head, int d, int p) 
+{
+    Node* start = (*head);
+    Node* temp = newNode(d, p);
+    if(*head != NULL) {
+        if ((*head)->priority > p) {
+            temp->next = *head;
+            (*head) = temp;
+        }
+        else {
+            while (start->next != NULL &&
+                start->next->priority < p) {
+                start = start->next;
+            }
+            temp->next = start->next;
+            start->next = temp;
+        }
+    }
+    else {
+        (*head) = temp;
+    }
+}
+
+void pushFifo(Node** head, int d, int p)
+{
+    Node* start = (*head);
+    Node* temp = newNode(d, p);
+    if(*head != NULL) {
+        while (start->next != NULL) {
+            start = start->next;
+        }
+        temp->next = start->next;
+        start->next = temp;
+    }
+    else {
+        (*head) = temp;
+    }
+}
+ 
+int isEmpty(Node** head)
+{ 
+    return (*head) == NULL; 
+}
+
+int queueSize(Node** head)
+{
+    int size = 0;
+    struct node *temp;
+    if(head == NULL)
+    {
+        return size;
+    }
+    temp = *head;
+    while(temp != NULL)
+    {
+        size += 1;
+        temp = temp -> next;
+    }
+    return size;
+}
+ 
---
+++ queue.h
@@ -0,0 +1,13 @@
+typedef struct node {
+    int data; 
+    int priority;
+    struct node* next; 
+} Node;
+Node* newNode(int d, int p);
+int peek(Node** head);
+Node* getNode(Node** head);
+void pop(Node** head);
+void push(Node** head, int d, int p);
+void pushFifo(Node** head, int d, int p);
+int isEmpty(Node** head);
+int queueSize(Node** head);
